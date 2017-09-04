53008e0be22d92c96ae2e8975f671c904f4ad242
ebeninca <ebeninca@ebeninca-VirtualBox>
2017-09-04 03:15:27 -0300

    Separado controller do app

--- src/main/java/com/test/beninca/Example.java
+++ src/main/java/com/test/beninca/Example.java
@@ -4,23 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @EnableAutoConfiguration
 @SpringBootApplication
 public class Example extends SpringBootServletInitializer {
-
-	@RequestMapping("/")
-	String home() {
-		return "Hello World!";
-	}
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Example.class, args);
 	}
 
-	
-	
 }
